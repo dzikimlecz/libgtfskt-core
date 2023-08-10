d3e7118949eaf18cabd07f439106cc5088544f81
dzikimlecz <j.pul312@gmail.com>
2023-08-10 19:10:12 +0200

    fixed bug of showing services after midnight not after current time

--- src/main/kotlin/FeedProcessing.kt
+++ src/main/kotlin/FeedProcessing.kt
@@ -6,6 +6,7 @@ import java.io.File
 import java.time.DayOfWeek
 import java.time.LocalDate
 import java.time.LocalDateTime
+import java.time.LocalTime
 import java.util.stream.Collectors
 
 
@@ -26,33 +27,44 @@ interface FeedProcessor {
 fun feedProcessor(feed: GtfsFeed): FeedProcessor = FeedProcessorImpl(feed)
 
 private class FeedProcessorImpl(val feed: GtfsFeed) : FeedProcessor {
-    override fun getUpcomingServicesFor(stop: String): List<UpcomingService> {
-        val today = LocalDate.now()
-        return feed.stopTimes.stream()
-            .filter { it.stop.name == stop }
+    override fun getUpcomingServicesFor(stop: String): List<UpcomingService> =
+        feed.stopTimes.stream()
+            .filter { it.stop.name.equals(stop.trim(), true) }
+            .filter(StopTime::isToday)
+            .map(StopTime::upcomingService)
             .filter {
-                when (val dayOfWeek = today.dayOfWeek) {
-                    DayOfWeek.MONDAY -> it.trip.service.monday
-                    DayOfWeek.TUESDAY -> it.trip.service.tuesday
-                    DayOfWeek.WEDNESDAY -> it.trip.service.wednesday
-                    DayOfWeek.THURSDAY -> it.trip.service.thursday
-                    DayOfWeek.FRIDAY -> it.trip.service.friday
-                    DayOfWeek.SATURDAY -> it.trip.service.saturday
-                    DayOfWeek.SUNDAY -> it.trip.service.sunday
-                    else -> throw IllegalStateException(
-                        "Unknown day of week value: $dayOfWeek"
-                    )
-                }
+                it.departure.toLocalTime()
+                    .isAfter(LocalTime.now().minusMinutes(1))
             }
-            .map {
-                UpcomingService(
-                    line = it.trip.route,
-                    stop = it.stop,
-                    departure = LocalDateTime.of(today,
-                        it.departureTime ?: it.arrivalTime!!
-                    ),
-                    direction = it.trip.headsign ?: it.trip.route.longName
-                )
-            }.collect(Collectors.toList())
-    }
+            .limit(20)
+            .collect(Collectors.toList())
+
+
+
+
+
 }
+
+private fun StopTime.upcomingService() = UpcomingService(
+    line = trip.route,
+    stop = stop,
+    departure = LocalDateTime.of(
+        LocalDate.now(),
+        departureTime ?: arrivalTime!!
+    ),
+    direction = trip.headsign ?: trip.route.longName
+)
+
+private fun StopTime.isToday() =
+    when (val dayOfWeek = LocalDate.now().dayOfWeek) {
+        DayOfWeek.MONDAY -> trip.service.monday
+        DayOfWeek.TUESDAY -> trip.service.tuesday
+        DayOfWeek.WEDNESDAY -> trip.service.wednesday
+        DayOfWeek.THURSDAY -> trip.service.thursday
+        DayOfWeek.FRIDAY -> trip.service.friday
+        DayOfWeek.SATURDAY -> trip.service.saturday
+        DayOfWeek.SUNDAY -> trip.service.sunday
+        else -> throw IllegalStateException(
+            "Unknown day of week value: $dayOfWeek"
+        )
+    }
